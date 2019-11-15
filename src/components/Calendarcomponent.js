cc43458c2391777425b7b251e0a1a497f0ec5366
Olli Eloranta <olli@ollieloranta.fi>
2019-11-15 18:07:22 +0200

    Trainings linked to calendar

--- src/components/Calendarcomponent.js
+++ src/components/Calendarcomponent.js
@@ -1,23 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import ReactTable from 'react-table';
-import 'react-table/react-table.css';
 import moment from 'moment';
-import Button from 'react-bootstrap/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import Modalcomponent from './Modalcomponent';
-import Dialogcomponent from './Dialogcomponent';
-import Toastcomponent from './Toastcomponent';
-import Addtraining from './Addtraining';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import '../CustomStyles.css';
 
 const Calendarcomponent = (props) => {
     const [trainings, setTrainings] = useState([]);
-    const [training, setTraining] = useState([{
-      starttime: '', endtime: '', title: '', allDay: false
-    }]);
 
     const fetchTrainings = () => {
       fetch('https://customerrest.herokuapp.com/gettrainings')
@@ -26,20 +14,16 @@ const Calendarcomponent = (props) => {
       .catch(err => console.error(err));
     }
 
+    const training = trainings.map((tr, index) => {
+          return {title: tr.activity, start: moment(tr.date).toDate(), end: moment(tr.date).add(4, "hours").toDate(), allDay: false}
+    });
+
+    console.log(training);
+  
+
     useEffect(() => {
         fetchTrainings();
-        trainings.map((tr) => {
-          setTraining({...training, starttime: tr.date, endtime: tr.date, title: tr.activity})
-        });
     }, []);
-
-    //const trainingsArray = trainings.map((tr) => {
-        //setTraining({...training, starttime: tr.date, endtime: tr.date, title: tr.activity})
-      //}
-    //);
-
-   // Object.entries(trainings).map(([k, v]) 
-    //);
     
     const localizer = momentLocalizer(moment);
     
